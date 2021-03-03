3ea3912ce9f17e546be20f7cdc70e63d9e14621f
Ana Trajkovska <ana@zeit.co>
2021-03-03 20:59:59 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Post() {
     <>
       <Image
         alt="Example image"
-        src="https://i.ytimg.com/vi/PKffm2uI4dk/maxresdefault.jpg"
+        src="https://cdn.truvvle.com//fill/400/100/sm/1/aHR0cHM6Ly9pbWcudHJhdmVsZmVlZC5pby9qcHBob3RvZ3JhcGh5JTJGMjAyMDExMjBUMTAyNzA3NzQ1Wi1qcGluYXNpYWxvZ28ucG5n"
         width={400}
         height={100}
       />
